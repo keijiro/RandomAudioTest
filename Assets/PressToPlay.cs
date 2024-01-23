4982148226e45d4286eb848057c661b09527c44a
Keijiro Takahashi <keijiro@gmail.com>
2024-01-23 15:01:53 +0900

    supersimplified

--- Assets/PressToPlay.cs
+++ Assets/PressToPlay.cs
@@ -1,30 +1,9 @@
 using UnityEngine;
-using UnityEngine.Audio;
 
 public sealed class PressToPlay : MonoBehaviour
 {
-    [field:SerializeField] public AudioResource Audio { get; set; }
     [field:SerializeField] public KeyCode Key { get; set; }
 
-    bool _active;
-
     void Update()
-    {
-        var src = GetComponent<AudioSource>();
-        if (Input.GetKey(Key))
-        {
-            if (!_active)
-            {
-                src.resource = Audio;
-                src.loop = true;
-                src.Play();
-                _active = true;
-            }
-        }
-        else
-        {
-            if (_active) src.Stop();
-            _active = false;
-        }
-    }
+      => GetComponent<AudioSource>().enabled = Input.GetKey(Key);
 }
